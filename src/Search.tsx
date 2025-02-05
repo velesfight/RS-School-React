0013c93ae7cd300d753d052df78a519b1ed1f71a
Ekaterina Vinnikova <red-white.katya@mail.ru>
2025-02-04 21:46:03 -0500

    change Search to functional

--- src/Search.tsx
+++ src/Search.tsx
@@ -1,43 +1,30 @@
-import React, { ChangeEvent } from 'react';
+import { useState, ChangeEvent } from 'react';
 
 interface SearchProps {
   onSearch: (query: string) => void;
 }
 
-interface SearchState {
-  searchQuery: string;
-}
+const Search = ({ onSearch }: SearchProps): JSX.Element => {
+  const [searchQuery, setSearchQuery] = useState<string>(
+    localStorage.getItem('searchQuery') || ''
+  );
 
-class Search extends React.Component<SearchProps, SearchState> {
-  constructor(props: SearchProps) {
-    super(props);
-    this.state = {
-      searchQuery: localStorage.getItem('searchQuery') || '',
-    };
-  }
-
-  handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    this.setState({ searchQuery: event.target.value });
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(event.target.value);
   };
 
-  handleSearch = () => {
-    const { searchQuery } = this.state;
-    localStorage.setItem('searchQuery', this.state.searchQuery);
-    this.props.onSearch(searchQuery);
+  const handleSearch = () => {
+    const trimQuery = searchQuery.trim();
+    localStorage.setItem('searchQuery', trimQuery);
+    onSearch(trimQuery);
   };
 
-  render() {
-    return (
-      <div>
-        <input
-          type="text"
-          value={this.state.searchQuery}
-          onChange={this.handleChange}
-        />
-        <button onClick={this.handleSearch}>Search</button>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <input type="text" value={searchQuery} onChange={handleChange} />
+      <button onClick={handleSearch}>Search</button>
+    </div>
+  );
+};
 
 export default Search;
