3e708bca468e1e67429e7a794cfb2025e457714c
Ekaterina Vinnikova <red-white.katya@mail.ru>
2025-02-09 15:09:41 -0500

    try

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './index.css';
-import App from './App.tsx';
-import ErrorBoundary from './ErrorBoundary.tsx';
-import Details from './Details.tsx';
+import App from './App';
+import ErrorBoundary from './ErrorBoundary';
+import Details from './Details';
+import React from 'react';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById('root')!).render(
